05f1be134d5258febc430d7cb005400967490fff
Melvin Gruschow and said ahmed <melvin@>
2017-01-11 15:40:38 -0800

    Final styling and image choices

---
+++ online-marketplace/js/scripts.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#buySneakers').submit(function(event){
+    var cost = $('input:radio[name=product]:checked').val();
+    // console.log(cost);
+    var name = $('#customerName').val();
+    var address = $('#customerAddress').val();
+    var totalCost = (parseInt(cost)*1.095).toFixed(2)+"$";
+    // console.log(totalCost);
+
+    $('#sentName').text(name);
+    $('#sentAddress').text(address);
+    $('#totalCost').text(totalCost);
+    $('#receipt').show();
+    event.preventDefault();
+  });
+})
